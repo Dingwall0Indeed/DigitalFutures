12601875e818a6e513dbf2787f4f3da2d04f1957
Dingwall0Indeed <Dingwall0Indeed@gmail.com>
2013-11-28 21:56:40 +1300

    Added JS for header movement
    Yeah, I'm about to go to bed, that's why I'm commiting now...

--- Website/js/index.js
+++ Website/js/index.js
@@ -1,40 +1,61 @@
-/**
- * Parallax Scrolling Tutorial
- * For NetTuts+
- *  
- * Author: Mohiuddin Parekh
- *	http://www.mohi.me
- * 	@mohiuddinparekh   
- */
-
-
 $(document).ready(function(){
 	// Cache the Window object
 	$window = $(window);
+	
+	window.onscroll = scroll;
+	
+	function scroll () {
+		/*if (true){
+			document.getElementById(nav_bar).style.top='0px';	
+		}
+		else {
+			
+		}*/
+	}
                 
-   $('section[data-type="background"]').each(function(){
-     var $bgobj = $(this); // assigning the object
+   $('section[data-type="background"]').each(function(){ 
+   // Iterate over all background types on the page
+   // That's everything with the 'data-type='background'' tag
+     	var $bgobj = $(this); // assigning the object
                     
-      $(window).scroll(function() {
+      	$(window).scroll(function() {
+					  
+		  // Scroll the background at var speed
+		  // the yPos is a negative value because we're scrolling it UP!								
+		  var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+		  
+		  // Put together our final background position
+		  var coords = '50% '+ yPos + 'px';
+  
+		  // Move the background
+		  $bgobj.css({ backgroundPosition: coords });
+		  
+		  
+		}); // window scroll Ends
+   });	        
+   $('div[data-type="_header"]').each(function(){ 
+   // Iterate over all background types on the page
+   // That's everything with the 'data-type='background'' tag
+     	var $bgobj = $(this); // assigning the object
                     
-		// Scroll the background at var speed
-		// the yPos is a negative value because we're scrolling it UP!								
-		var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-		
-		// Put together our final background position
-		var coords = '50% '+ yPos + 'px';
-
-		// Move the background
-		$bgobj.css({ backgroundPosition: coords });
-		
-}); // window scroll Ends
-
- });	
+      	$(window).scroll(function() {
+					  
+		  // Scroll the background at var speed
+		  // the yPos is a negative value because we're scrolling it UP!								
+		  var yPos = ($window.scrollTop() / 0.5); 								
+		  yPos = '0px'; 
+		  $bgobj.css({ top: yPos });
+		  
+		  
+		}); // window scroll Ends
+   });	
+   
 
 }); 
+
+
 /* 
  * Create HTML5 elements for IE's sake
  */
-
 document.createElement("article");
 document.createElement("section");
